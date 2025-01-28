db8e1d6fe19f9efcea5e65fece1ad60c6d242ca7
jordy <marcelo2005jmsp@gmail.com>
2025-01-28 10:25:36 -0500

    proyecto creado

---
+++ Sistema_Gestion_Tareas/src/main/java/ec/edu/espoch/sistema_gestion_tareas/Sistema_Gestion_Tareas.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.sistema_gestion_tareas;
+
+import ec.edu.espoch.sistema_gestion_tareas.vista.Vista;
+
+/**
+ *
+ * @author jordy
+ */
+public class Sistema_Gestion_Tareas {
+
+    public static void main(String[] args) {
+        Vista Derivadas=new Vista();
+        Derivadas.setVisible(true);
+    }
+}
